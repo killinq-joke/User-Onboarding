2aee9b7bd0ad0d93d515fd7861d07a0d1d00df90
Zak <zakadmaster@gmail.com>
2020-01-30 15:19:17 +0100

    Addition of api post

--- user-onboarding/src/NormalForm.js
+++ user-onboarding/src/NormalForm.js
@@ -3,11 +3,12 @@ import * as Yup from 'yup';
 import { Formik, Form, Field, ErrorMessage} from 'formik';
 import axios from 'axios';
 
+
 function NormalForm() {
 
     function handleSubmit(values, actions) {
-        console.log(values);
-        console.log(actions);
+        // console.log(values);
+        // console.log(actions);
 
         axios
         .post('https://reqres.in/api/users/', values)
@@ -25,7 +26,7 @@ function NormalForm() {
         name: Yup.string().required('Please enter your name'),
         email: Yup.string().required('Please enter your email'),
         password: Yup.string().required('Please enter a password'),
-        terms_of_service: Yup.boolean()
+        terms_of_service: Yup.boolean().oneOf([true], 'Field must be checked')
       });
       
       const initialState = {
@@ -36,39 +37,44 @@ function NormalForm() {
       }
 
   return (
-      <div>
+      <div >
           <Formik
+          
           onSubmit={handleSubmit}
           initialValues={initialState}
           validationSchema={validationSchema}
           >
-              <Form>
+              <Form className="form">
                   <label htmlFor="name">Name</label>
                   <Field 
                     type="text"
                     id="name"
                     name="name"
-                    placeholder="Enter your name here"/>
+                    placeholder="Enter your name here"
+                    className="input"/>
                     <ErrorMessage name="name" component="div" className="error"/>
                   <label htmlFor="email">Email</label>
                   <Field 
                     type="text"
                     id="email"
                     name="email"
-                    placeholder="Enter your email here"/>
+                    placeholder="Enter your email here"
+                    className="input"/>
                     <ErrorMessage name="email" component="div" className="error"/>
                   <label htmlFor="password">Password</label>
                   <Field 
                     type="password"
                     id="password"
                     name="password"
-                    placeholder="Enter your password here"/>
+                    placeholder="Enter your password here"
+                    className="input"/>
                     <ErrorMessage name="password" component="div" className="error"/>
                   <label htmlFor="terms_of_service">Terms of Service</label>
                   <Field 
                     type="checkbox"
                     id="terms_of_service"
-                    name="terms_of_service"/>
+                    name="terms_of_service"
+                    className="input"/>
                     <ErrorMessage name="terms_of_service" component="div" className="error"/>
                     <button type="submit">submit</button>
               </Form>
